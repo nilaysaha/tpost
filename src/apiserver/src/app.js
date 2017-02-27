d78008a48870d794bfda5a3c778e2d4ee7021d04
Nilay Saha <nilay.saha@intive-kupferwerk.com>
2017-02-27 16:43:49 +0100

    cors should include http

--- src/apiserver/src/app.js
+++ src/apiserver/src/app.js
@@ -17,7 +17,7 @@ exports = module.exports = function(assert,restify,plugins,routes) {
   server.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
